593a48e0c3d284e00979ee0fe5a1770d55215cda
EdenItz <edenitz34@gmail.com>
2022-12-02 16:57:38 +0200

    add turns

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,9 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean XTurn = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +31,21 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView playerTurn = (ImageView) findViewById(R.id.tiktaktoe_text);
                 System.out.println("pressed button: " + cellInx);
-                btn.setImageResource(R.drawable.x);
+                if(true) {
+                    if (XTurn) {
+                        btn.setImageResource(R.drawable.x);
+                        playerTurn.setImageResource(R.drawable.oplay);
+                    } else {
+                        btn.setImageResource(R.drawable.o);
+                        playerTurn.setImageResource(R.drawable.xplay);
+                    }
+                }
+                XTurn = !XTurn;
+
+
+
                 // Do whatever you want(str can be used here)
 
             }
